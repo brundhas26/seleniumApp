51e9d8c67ba8c424710ebdb3693695a4c0baad9c
brundhas26 <brundas265@gmail.com>
2026-05-11 13:35:51 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,21 +3,16 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
-public class App {
-
-    public static void main(String[] args) {
-
-        ChromeOptions options = new ChromeOptions();
-
-        options.addArguments("--headless=new");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-
-        WebDriver driver = new ChromeDriver(options);
+public class App
+{
+    public static void main(String[] args)
+    {
+        WebDriver driver = new ChromeDriver();
 
         driver.get("https://www.saucedemo.com/");
+
+        driver.manage().window().maximize();
 
         driver.findElement(By.id("user-name"))
               .sendKeys("standard_user");
@@ -27,12 +22,5 @@ public class App {
 
         driver.findElement(By.id("login-button"))
               .click();
-
-        // Print current page URL
-        System.out.println("Current URL: " + driver.getCurrentUrl());
-
-        System.out.println("Login Successful");
-
-        driver.quit();
     }
 }
